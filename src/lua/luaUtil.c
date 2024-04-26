7953c3b989dd61b0e090644bc8d791157ee624ea
danihek <danihek07@gmail.com>
2024-04-26 18:50:16 +0200

    lua integration: added functions

--- src/lua/luaUtil.c
+++ src/lua/luaUtil.c
@@ -3,3 +3,45 @@
 #include <lauxlib.h>
 
 #include "../../include/lua/luaUtil.h"
+
+lua_State *initLua()
+{
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    return L;
+}
+
+int luaRunCode(lua_State *L, char *code)
+{
+  if (luaL_loadstring(L, code) == LUA_OK)
+  {
+    if (lua_pcall(L, 0, 0, 0) == LUA_OK)
+    {
+      lua_pop(L, lua_gettop(L));
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
+  }
+  return 1;
+}
+
+int luaRunFromFile(lua_State *L, char *filename)
+{
+    if (luaL_dofile(L, filename) == LUA_OK)
+    {
+        lua_pop(L, lua_gettop(L));
+        return 0;
+    }
+    return 1;
+}
+
+void test()
+{
+    lua_State *L = initLua();
+    luaRunCode(L,"print('Hello, World')");
+    luaRunCode(L, "test_config.lua");
+    lua_close(L);
+}
---
+++ src/lua/test_config.lua
@@ -0,0 +1,13 @@
+keyboard = {
+  delay = 50,
+  rate = 200,
+  layout = "us"
+}
+
+monitor = {
+  width = 2560,
+  height = 1440,
+  hz = 165
+}
+
+print("from lua")
